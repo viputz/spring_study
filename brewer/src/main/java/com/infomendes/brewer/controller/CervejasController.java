4eaa210112a976ee3276a8fea8e53e0f6bea393c
Vinicius Coutinho <mingal.rox@gmail.com>
2019-06-10 22:35:29 -0300

    primeiro commit

---
+++ brewer/src/main/java/com/infomendes/brewer/controller/CervejasController.java
@@ -0,0 +1,41 @@
+package com.infomendes.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.infomendes.brewer.model.Cerveja;
+
+@Controller
+public class CervejasController {
+
+	@GetMapping("/cervejas/novo")
+	public String novo(Cerveja cerveja) {
+		return "cerveja/CadastroCerveja";
+	}
+
+	@PostMapping("/cervejas/novo")
+	public String cadastrar(@Validated Cerveja cerveja, BindingResult result, RedirectAttributes attributes) {
+
+		if (result.hasErrors()) {
+			return novo(cerveja);
+		}
+
+		// salvar
+		attributes.addFlashAttribute("mensagem", "Cadastrado com sucesso");
+		System.out.println(">>>>>>> Ok:" + cerveja.getSku());
+
+		return "redirect:/cervejas/novo";
+
+	}
+
+	@RequestMapping("/cervejas/cadastro")
+	public String cadastro() {
+		return "cerveja/cadastro-produto";
+	}
+
+}
